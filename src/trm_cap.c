b7a788bb9618ac5538e52415f689df3674d5af3f
Thomas Mertes <thomas.mertes@gmx.at>
2025-09-16 08:46:10 +0200

    Refactor my_tgetent() to interpret terminal_name as name

--- src/trm_cap.c
+++ src/trm_cap.c
@@ -52,6 +52,8 @@
 /* Configuration: */
 #undef EMULATE_TERMCAP
 
+#define CAPBUF_SIZE 2048
+
 
 #ifdef C_PLUS_PLUS
 
@@ -84,8 +86,6 @@ int tputs (char *, int, int (*) (char ch));
 #define SEARCHED_BUFFER_SIZE    33
 #define CAP_VALUE_BUFFER_SIZE 1024
 
-#define CAPBUF_SIZE 2048
-
 char capabilities[CAPBUF_SIZE + NULL_TERMINATION_LEN];
 
 
@@ -93,21 +93,43 @@ char capabilities[CAPBUF_SIZE + NULL_TERMINATION_LEN];
 int my_tgetent (char *capbuf, char *terminal_name)
 
   {
+    char *home_dir_path;
+    memSizeType file_name_size;
+    char *file_name;
     FILE *term_descr_file;
     int len;
     int result;
 
   /* my_tgetent */
-    if ((term_descr_file = fopen(terminal_name, "r")) != NULL) {
-      len = fread(capbuf, 1, CAPBUF_SIZE, term_descr_file);
-      capbuf[len] = '\0';
-      fclose(term_descr_file);
-      result = 0;
-    } else {
-      capbuf[0] = '\0';
-      result = -1;
+    home_dir_path = getenv("HOME");
+    if (home_dir_path == NULL) {
+      home_dir_path = "";
     } /* if */
-    strcpy(capabilities, capbuf);
+    if (terminal_name == NULL) {
+      terminal_name = "";
+    } /* if */
+    file_name_size = strlen(home_dir_path) + 6 + strlen(terminal_name);
+    file_name = malloc(file_name_size + 1);
+    if (file_name != NULL) {
+      strcpy(file_name, home_dir_path);
+      len = strlen(file_name);
+      if (len > 0 && file_name[len - 1] != '/') {
+        file_name[len] = '/';
+        len++;
+      } /* if */
+      strcpy(&file_name[len], ".term");
+      strcat(file_name, terminal_name);
+      if ((term_descr_file = fopen(file_name, "r")) != NULL) {
+        len = fread(capabilities, 1, CAPBUF_SIZE, term_descr_file);
+        capabilities[len] = '\0';
+        fclose(term_descr_file);
+        result = 0;
+      } else {
+        capabilities[0] = '\0';
+        result = -1;
+      } /* if */
+      free(file_name);
+    } /* if */
     return result;
   } /* my_tgetent */
 
@@ -254,179 +276,153 @@ int getcaps (void)
 
   {
     static char capbuf[CAPBUF_SIZE + NULL_TERMINATION_LEN];
-    int return_code;
+    int tgetent_result;
     char *area;
-    char *home_dir_path;
     char *terminal_name;
-    char term_descr_file_name[256];
-    int len;
 
   /* getcaps */
     if (!caps_initialized) {
       terminal_name = getenv("TERM");
-#ifdef EMULATE_TERMCAP
-      home_dir_path = getenv("HOME");
-      if (home_dir_path != NULL) {
-        strcpy(term_descr_file_name, home_dir_path);
-        len = strlen(term_descr_file_name);
-      } else {
-        len = 0;
+      tgetent_result = tgetent(capbuf, terminal_name);
+      /* printf("tgetent -> %d\n", tgetent_result); */
+      if (tgetent_result == 1) {
+        area = capbuf;
+        insert_line =           tgetstr("al", &area);
+        auto_right_margin =    tgetflag("am");
+        BC =                    tgetstr("bc", &area);
+        clr_eos =               tgetstr("cd", &area);
+        clr_eol =               tgetstr("ce", &area);
+        clear_screen =          tgetstr("cl", &area);
+        columns =               tgetnum("co");
+        cursor_address =        tgetstr("cm", &area);
+        carriage_return =       tgetstr("cr", &area);
+        delete_character =      tgetstr("dc", &area);
+        delete_line =           tgetstr("dl", &area);
+        enter_delete_mode =     tgetstr("dm", &area);
+        cursor_down =           tgetstr("do", &area);
+        exit_delete_mode =      tgetstr("ed", &area);
+        exit_insert_mode =      tgetstr("ei", &area);
+        hard_copy =            tgetflag("hc");
+        insert_character =      tgetstr("ic", &area);
+        enter_insert_mode =     tgetstr("im", &area);
+        IP =                    tgetstr("ip", &area);
+        cursor_left =           tgetstr("le", &area);
+        lines =                 tgetnum("li");
+        move_insert_mode =     tgetflag("mi");
+        cursor_right =          tgetstr("nd", &area);
+        pad_char =              tgetstr("pc", &area);
+        cursor_normal =         tgetstr("ve", &area);
+        exit_standout_mode =    tgetstr("se", &area);
+        enter_standout_mode =   tgetstr("so", &area);
+        exit_ca_mode =          tgetstr("te", &area);
+        enter_ca_mode =         tgetstr("ti", &area);
+        cursor_up =             tgetstr("up", &area);
+        cursor_visible =        tgetstr("vs", &area);
+        cursor_invisible =      tgetstr("vi", &area);
+        enter_reverse_mode =    tgetstr("mr", &area);
+        ceol_standout_glitch = tgetflag("xs");
+        parm_insert_line =      tgetstr("AL", &area);
+        parm_delete_line =      tgetstr("DL", &area);
+        keypad_local =          tgetstr("ke", &area);
+        keypad_xmit =           tgetstr("ks", &area);
+        key_backspace =         tgetstr("kb", &area);
+        key_btab =              tgetstr("kB", &area);
+        key_b2 =                tgetstr("K2", &area);
+        key_catab =             tgetstr("ka", &area);
+        key_clear =             tgetstr("kC", &area);
+        key_ctab =              tgetstr("kt", &area);
+        key_dc =                tgetstr("kD", &area);
+        key_dl =                tgetstr("kL", &area);
+        key_down =              tgetstr("kd", &area);
+        key_eic =               tgetstr("kM", &area);
+        key_end =               tgetstr("@7", &area);
+        key_enter =             tgetstr("@8", &area);
+        key_eol =               tgetstr("kE", &area);
+        key_eos =               tgetstr("kS", &area);
+        key_f0 =                tgetstr("k0", &area);
+        key_f1 =                tgetstr("k1", &area);
+        key_f2 =                tgetstr("k2", &area);
+        key_f3 =                tgetstr("k3", &area);
+        key_f4 =                tgetstr("k4", &area);
+        key_f5 =                tgetstr("k5", &area);
+        key_f6 =                tgetstr("k6", &area);
+        key_f7 =                tgetstr("k7", &area);
+        key_f8 =                tgetstr("k8", &area);
+        key_f9 =                tgetstr("k9", &area);
+        key_f10 =               tgetstr("k;", &area);
+        key_f11 =               tgetstr("F1", &area);
+        key_f12 =               tgetstr("F2", &area);
+        key_f13 =               tgetstr("F3", &area);
+        key_f14 =               tgetstr("F4", &area);
+        key_f15 =               tgetstr("F5", &area);
+        key_f16 =               tgetstr("F6", &area);
+        key_f17 =               tgetstr("F7", &area);
+        key_f18 =               tgetstr("F8", &area);
+        key_f19 =               tgetstr("F9", &area);
+        key_f20 =               tgetstr("FA", &area);
+        key_f21 =               tgetstr("FB", &area);
+        key_f22 =               tgetstr("FC", &area);
+        key_f23 =               tgetstr("FD", &area);
+        key_f24 =               tgetstr("FE", &area);
+        key_f25 =               tgetstr("FF", &area);
+        key_f26 =               tgetstr("FG", &area);
+        key_f27 =               tgetstr("FH", &area);
+        key_f28 =               tgetstr("FI", &area);
+        key_f29 =               tgetstr("FJ", &area);
+        key_f30 =               tgetstr("FK", &area);
+        key_f30 =               tgetstr("FK", &area);
+        key_f31 =               tgetstr("FL", &area);
+        key_f32 =               tgetstr("FM", &area);
+        key_f33 =               tgetstr("FN", &area);
+        key_f34 =               tgetstr("FO", &area);
+        key_f35 =               tgetstr("FP", &area);
+        key_f36 =               tgetstr("FQ", &area);
+        key_f37 =               tgetstr("FR", &area);
+        key_f38 =               tgetstr("FS", &area);
+        key_f39 =               tgetstr("FT", &area);
+        key_f40 =               tgetstr("FU", &area);
+        key_f41 =               tgetstr("FV", &area);
+        key_f42 =               tgetstr("FW", &area);
+        key_f43 =               tgetstr("FX", &area);
+        key_f44 =               tgetstr("FY", &area);
+        key_f45 =               tgetstr("FZ", &area);
+        key_f46 =               tgetstr("Fa", &area);
+        key_f47 =               tgetstr("Fb", &area);
+        key_f48 =               tgetstr("Fc", &area);
+        key_f49 =               tgetstr("Fd", &area);
+        key_f50 =               tgetstr("Fe", &area);
+        key_f51 =               tgetstr("Ff", &area);
+        key_f52 =               tgetstr("Fg", &area);
+        key_f53 =               tgetstr("Fh", &area);
+        key_f54 =               tgetstr("Fi", &area);
+        key_f55 =               tgetstr("Fj", &area);
+        key_f56 =               tgetstr("Fk", &area);
+        key_f57 =               tgetstr("Fl", &area);
+        key_f58 =               tgetstr("Fm", &area);
+        key_f59 =               tgetstr("Fn", &area);
+        key_f60 =               tgetstr("Fo", &area);
+        key_f61 =               tgetstr("Fp", &area);
+        key_f62 =               tgetstr("Fq", &area);
+        key_f63 =               tgetstr("Fr", &area);
+        key_find =              tgetstr("@0", &area);
+        key_home =              tgetstr("kh", &area);
+        key_ic =                tgetstr("kI", &area);
+        key_il =                tgetstr("kA", &area);
+        key_left =              tgetstr("kl", &area);
+        key_ll =                tgetstr("kH", &area);
+        key_npage =             tgetstr("kN", &area);
+        key_ppage =             tgetstr("kP", &area);
+        key_right =             tgetstr("kr", &area);
+        key_select=             tgetstr("*6", &area);
+        key_sf =                tgetstr("kF", &area);
+        key_sr =                tgetstr("kR", &area);
+        key_stab =              tgetstr("kT", &area);
+        key_up =                tgetstr("ku", &area);
+        if (cursor_address == NULL) {
+          printf("cursor_address == NULL\n");
+        } /* if */
+        caps_initialized = TRUE;
       } /* if */
-      if (len > 0 && term_descr_file_name[len - 1] != '/') {
-        term_descr_file_name[len] = '/';
-        len++;
-      } /* if */
-      strcpy(&term_descr_file_name[len], ".term");
-      if (terminal_name != NULL) {
-        strcat(term_descr_file_name, terminal_name);
-      } /* if */
-/*    setupterm(terminal_name, os_fileno(stdout), &err); */
-/* printf(":%s:%s:\n", terminal_name, term_descr_file_name); */
-      return_code = tgetent(capbuf, term_descr_file_name);
-#else
-      return_code = tgetent(capbuf, terminal_name);
-#endif
-/*
-printf(":%d:\n", return_code);
-printf(":%d:\n", strlen(capbuf));
-printf("\"%s\"\n", capbuf);
-*/
-      area = capbuf;
-      insert_line =           tgetstr("al", &area);
-      auto_right_margin =    tgetflag("am");
-      BC =                    tgetstr("bc", &area);
-      clr_eos =               tgetstr("cd", &area);
-      clr_eol =               tgetstr("ce", &area);
-      clear_screen =          tgetstr("cl", &area);
-      columns =               tgetnum("co");
-      cursor_address =        tgetstr("cm", &area);
-      carriage_return =       tgetstr("cr", &area);
-      delete_character =      tgetstr("dc", &area);
-      delete_line =           tgetstr("dl", &area);
-      enter_delete_mode =     tgetstr("dm", &area);
-      cursor_down =           tgetstr("do", &area);
-      exit_delete_mode =      tgetstr("ed", &area);
-      exit_insert_mode =      tgetstr("ei", &area);
-      hard_copy =            tgetflag("hc");
-      insert_character =      tgetstr("ic", &area);
-      enter_insert_mode =     tgetstr("im", &area);
-      IP =                    tgetstr("ip", &area);
-      cursor_left =           tgetstr("le", &area);
-      lines =                 tgetnum("li");
-      move_insert_mode =     tgetflag("mi");
-      cursor_right =          tgetstr("nd", &area);
-      pad_char =              tgetstr("pc", &area);
-      cursor_normal =         tgetstr("ve", &area);
-      exit_standout_mode =    tgetstr("se", &area);
-      enter_standout_mode =   tgetstr("so", &area);
-      exit_ca_mode =          tgetstr("te", &area);
-      enter_ca_mode =         tgetstr("ti", &area);
-      cursor_up =             tgetstr("up", &area);
-      cursor_visible =        tgetstr("vs", &area);
-      cursor_invisible =      tgetstr("vi", &area);
-      enter_reverse_mode =    tgetstr("mr", &area);
-      ceol_standout_glitch = tgetflag("xs");
-      parm_insert_line =      tgetstr("AL", &area);
-      parm_delete_line =      tgetstr("DL", &area);
-      keypad_local =          tgetstr("ke", &area);
-      keypad_xmit =           tgetstr("ks", &area);
-      key_backspace =         tgetstr("kb", &area);
-      key_btab =              tgetstr("kB", &area);
-      key_b2 =                tgetstr("K2", &area);
-      key_catab =             tgetstr("ka", &area);
-      key_clear =             tgetstr("kC", &area);
-      key_ctab =              tgetstr("kt", &area);
-      key_dc =                tgetstr("kD", &area);
-      key_dl =                tgetstr("kL", &area);
-      key_down =              tgetstr("kd", &area);
-      key_eic =               tgetstr("kM", &area);
-      key_end =               tgetstr("@7", &area);
-      key_enter =             tgetstr("@8", &area);
-      key_eol =               tgetstr("kE", &area);
-      key_eos =               tgetstr("kS", &area);
-      key_f0 =                tgetstr("k0", &area);
-      key_f1 =                tgetstr("k1", &area);
-      key_f2 =                tgetstr("k2", &area);
-      key_f3 =                tgetstr("k3", &area);
-      key_f4 =                tgetstr("k4", &area);
-      key_f5 =                tgetstr("k5", &area);
-      key_f6 =                tgetstr("k6", &area);
-      key_f7 =                tgetstr("k7", &area);
-      key_f8 =                tgetstr("k8", &area);
-      key_f9 =                tgetstr("k9", &area);
-      key_f10 =               tgetstr("k;", &area);
-      key_f11 =               tgetstr("F1", &area);
-      key_f12 =               tgetstr("F2", &area);
-      key_f13 =               tgetstr("F3", &area);
-      key_f14 =               tgetstr("F4", &area);
-      key_f15 =               tgetstr("F5", &area);
-      key_f16 =               tgetstr("F6", &area);
-      key_f17 =               tgetstr("F7", &area);
-      key_f18 =               tgetstr("F8", &area);
-      key_f19 =               tgetstr("F9", &area);
-      key_f20 =               tgetstr("FA", &area);
-      key_f21 =               tgetstr("FB", &area);
-      key_f22 =               tgetstr("FC", &area);
-      key_f23 =               tgetstr("FD", &area);
-      key_f24 =               tgetstr("FE", &area);
-      key_f25 =               tgetstr("FF", &area);
-      key_f26 =               tgetstr("FG", &area);
-      key_f27 =               tgetstr("FH", &area);
-      key_f28 =               tgetstr("FI", &area);
-      key_f29 =               tgetstr("FJ", &area);
-      key_f30 =               tgetstr("FK", &area);
-      key_f30 =               tgetstr("FK", &area);
-      key_f31 =               tgetstr("FL", &area);
-      key_f32 =               tgetstr("FM", &area);
-      key_f33 =               tgetstr("FN", &area);
-      key_f34 =               tgetstr("FO", &area);
-      key_f35 =               tgetstr("FP", &area);
-      key_f36 =               tgetstr("FQ", &area);
-      key_f37 =               tgetstr("FR", &area);
-      key_f38 =               tgetstr("FS", &area);
-      key_f39 =               tgetstr("FT", &area);
-      key_f40 =               tgetstr("FU", &area);
-      key_f41 =               tgetstr("FV", &area);
-      key_f42 =               tgetstr("FW", &area);
-      key_f43 =               tgetstr("FX", &area);
-      key_f44 =               tgetstr("FY", &area);
-      key_f45 =               tgetstr("FZ", &area);
-      key_f46 =               tgetstr("Fa", &area);
-      key_f47 =               tgetstr("Fb", &area);
-      key_f48 =               tgetstr("Fc", &area);
-      key_f49 =               tgetstr("Fd", &area);
-      key_f50 =               tgetstr("Fe", &area);
-      key_f51 =               tgetstr("Ff", &area);
-      key_f52 =               tgetstr("Fg", &area);
-      key_f53 =               tgetstr("Fh", &area);
-      key_f54 =               tgetstr("Fi", &area);
-      key_f55 =               tgetstr("Fj", &area);
-      key_f56 =               tgetstr("Fk", &area);
-      key_f57 =               tgetstr("Fl", &area);
-      key_f58 =               tgetstr("Fm", &area);
-      key_f59 =               tgetstr("Fn", &area);
-      key_f60 =               tgetstr("Fo", &area);
-      key_f61 =               tgetstr("Fp", &area);
-      key_f62 =               tgetstr("Fq", &area);
-      key_f63 =               tgetstr("Fr", &area);
-      key_find =              tgetstr("@0", &area);
-      key_home =              tgetstr("kh", &area);
-      key_ic =                tgetstr("kI", &area);
-      key_il =                tgetstr("kA", &area);
-      key_left =              tgetstr("kl", &area);
-      key_ll =                tgetstr("kH", &area);
-      key_npage =             tgetstr("kN", &area);
-      key_ppage =             tgetstr("kP", &area);
-      key_right =             tgetstr("kr", &area);
-      key_select=             tgetstr("*6", &area);
-      key_sf =                tgetstr("kF", &area);
-      key_sr =                tgetstr("kR", &area);
-      key_stab =              tgetstr("kT", &area);
-      key_up =                tgetstr("ku", &area);
-      if (cursor_address == NULL) {
-        printf("cursor_address == NULL\n");
-      } /* if */
-      caps_initialized = TRUE;
     } /* if */
 /*  printf("%d <%s>\n", strlen(cursor_address), cursor_address); */
     return caps_initialized;
