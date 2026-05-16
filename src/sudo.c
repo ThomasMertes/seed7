61e976e6d17cbed2b2dbd0cf4009b5556875f769
Thomas Mertes <thomas.mertes@gmx.at>
2026-05-16 12:42:16 +0200

    Free fileName in success path

--- src/sudo.c
+++ src/sudo.c
@@ -148,6 +148,7 @@ int main (int argc, char *argv[])
             mainResult = -1;
           } /* if */
         } /* if */
+        free(fileName);
         free(parameters);
       } /* if */
     } /* if */
